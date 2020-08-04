67384c716ea14608c08e08d6b72aa06c59e18400
DoodleBears <yangmufeng233@gmail.com>
2020-08-04 11:09:56 +0800

    update echarts with true data

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -121,6 +121,10 @@ export default {
             this.newslist = res.data.newslist;
             this.showlist = this.newslist.slice(0,6);
             var maplist = [];
+            var xAxis = [];
+            var series1 = [];
+            var series2 = [];
+            var series3 = [];
             for(var i = 0; i < this.newslist.length; i++)
             {
                 var obj = {};
@@ -129,6 +133,16 @@ export default {
 
                 maplist.push(obj);
             }
+
+            for(var i = 0 ; i < 6; i++)
+            {
+                xAxis.push(this.newslist[i].provinceShortName);
+                series1.push(this.newslist[i].confirmedCount);
+                series2.push(this.newslist[i].deadCount);
+                series3.push(this.newslist[i].curedCount);
+
+            }
+
             var myChart = echarts.init(document.getElementById("map"));
 
             var option = {
@@ -169,6 +183,158 @@ export default {
 
             myChart.setOption(option);
 
+            //柱状图（顶部）
+            var myChart2 = echarts.init(document.getElementById('bar'));
+
+            // 指定图表的配置项和数据
+            var posList = [
+                'left', 'right', 'top', 'bottom',
+                'inside',
+                'insideTop', 'insideLeft', 'insideRight', 'insideBottom',
+                'insideTopLeft', 'insideTopRight', 'insideBottomLeft', 'insideBottomRight'
+            ];
+
+            var app = {};
+
+            app.configParameters = {
+                rotate: {
+                    min: -90,
+                    max: 90
+                },
+                align: {
+                    options: {
+                        left: 'left',
+                        center: 'center',
+                        right: 'right'
+                    }
+                },
+                verticalAlign: {
+                    options: {
+                        top: 'top',
+                        middle: 'middle',
+                        bottom: 'bottom'
+                    }
+                },
+                position: {
+                    options: echarts.util.reduce(posList, function (map, pos) {
+                        map[pos] = pos;
+                        return map;
+                    }, {})
+                },
+                distance: {
+                    min: 0,
+                    max: 100
+                }
+            };
+
+            app.config = {
+                rotate: 90,
+                align: 'left',
+                verticalAlign: 'middle',
+                position: 'insideBottom',
+                distance: 15,
+                onChange: function () {
+                    var labelOption = {
+                        normal: {
+                            rotate: app.config.rotate,
+                            align: app.config.align,
+                            verticalAlign: app.config.verticalAlign,
+                            position: app.config.position,
+                            distance: app.config.distance
+                        }
+                    };
+                    myChart.setOption({
+                        series: [{
+                            label: labelOption
+                        }, {
+                            label: labelOption
+                        }, {
+                            label: labelOption
+                        }, {
+                            label: labelOption
+                        }]
+                    });
+                }
+            };
+
+
+            var labelOption = {
+                show: true,
+                position: app.config.position,
+                distance: app.config.distance,
+                align: app.config.align,
+                verticalAlign: app.config.verticalAlign,
+                rotate: app.config.rotate,
+                formatter: '{c}  {name|{a}}',
+                fontSize: 16,
+                rich: {
+                    name: {
+                        textBorderColor: '#fff'
+                    }
+                }
+            };
+
+            var optionBar = {
+                color: ['#003366', '#006699', '#4cabce', '#e5323e'],
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'shadow'
+                    }
+                },
+                legend: {
+
+                },
+                toolbox: {
+                    show: true,
+                    orient: 'vertical',
+                    left: 'right',
+                    top: 'center',
+                    feature: {
+                        mark: {show: true},
+                        dataView: {show: true, readOnly: false},
+                        magicType: {show: true, type: ['line', 'bar', 'stack', 'tiled']},
+                        restore: {show: true},
+                        saveAsImage: {show: true}
+                    }
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        axisTick: {show: false},
+                        data: xAxis
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value'
+                    }
+                ],
+                series: [
+                    {
+                        name: '确诊',
+                        type: 'bar',
+                        barGap: 0,
+                        label: labelOption,
+                        data: series1
+                    },
+                    {
+                        name: '死亡',
+                        type: 'bar',
+                        label: labelOption,
+                        data: series2
+                    },
+                    {
+                        name: '治愈',
+                        type: 'bar',
+                        label: labelOption,
+                        data: series3
+                    }
+                ]
+            };
+            // 使用刚指定的配置项和数据显示图表。
+            myChart2.setOption(optionBar);
+
         })
         .catch(res=>{
 
@@ -176,165 +342,6 @@ export default {
     },
     mounted(){
 
-        
-
-
-        var myChart2 = echarts.init(document.getElementById('bar'));
-
-        // 指定图表的配置项和数据
-        var posList = [
-            'left', 'right', 'top', 'bottom',
-            'inside',
-            'insideTop', 'insideLeft', 'insideRight', 'insideBottom',
-            'insideTopLeft', 'insideTopRight', 'insideBottomLeft', 'insideBottomRight'
-        ];
-
-        var app = {};
-
-        app.configParameters = {
-            rotate: {
-                min: -90,
-                max: 90
-            },
-            align: {
-                options: {
-                    left: 'left',
-                    center: 'center',
-                    right: 'right'
-                }
-            },
-            verticalAlign: {
-                options: {
-                    top: 'top',
-                    middle: 'middle',
-                    bottom: 'bottom'
-                }
-            },
-            position: {
-                options: echarts.util.reduce(posList, function (map, pos) {
-                    map[pos] = pos;
-                    return map;
-                }, {})
-            },
-            distance: {
-                min: 0,
-                max: 100
-            }
-        };
-
-        app.config = {
-            rotate: 90,
-            align: 'left',
-            verticalAlign: 'middle',
-            position: 'insideBottom',
-            distance: 15,
-            onChange: function () {
-                var labelOption = {
-                    normal: {
-                        rotate: app.config.rotate,
-                        align: app.config.align,
-                        verticalAlign: app.config.verticalAlign,
-                        position: app.config.position,
-                        distance: app.config.distance
-                    }
-                };
-                myChart.setOption({
-                    series: [{
-                        label: labelOption
-                    }, {
-                        label: labelOption
-                    }, {
-                        label: labelOption
-                    }, {
-                        label: labelOption
-                    }]
-                });
-            }
-        };
-
-
-        var labelOption = {
-            show: true,
-            position: app.config.position,
-            distance: app.config.distance,
-            align: app.config.align,
-            verticalAlign: app.config.verticalAlign,
-            rotate: app.config.rotate,
-            formatter: '{c}  {name|{a}}',
-            fontSize: 16,
-            rich: {
-                name: {
-                    textBorderColor: '#fff'
-                }
-            }
-        };
-
-        var optionBar = {
-            color: ['#003366', '#006699', '#4cabce', '#e5323e'],
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'shadow'
-                }
-            },
-            legend: {
-                data: ['Forest', 'Steppe', 'Desert', 'Wetland']
-            },
-            toolbox: {
-                show: true,
-                orient: 'vertical',
-                left: 'right',
-                top: 'center',
-                feature: {
-                    mark: {show: true},
-                    dataView: {show: true, readOnly: false},
-                    magicType: {show: true, type: ['line', 'bar', 'stack', 'tiled']},
-                    restore: {show: true},
-                    saveAsImage: {show: true}
-                }
-            },
-            xAxis: [
-                {
-                    type: 'category',
-                    axisTick: {show: false},
-                    data: ['2012', '2013', '2014', '2015', '2016']
-                }
-            ],
-            yAxis: [
-                {
-                    type: 'value'
-                }
-            ],
-            series: [
-                {
-                    name: 'Forest',
-                    type: 'bar',
-                    barGap: 0,
-                    label: labelOption,
-                    data: [320, 332, 301, 334, 390]
-                },
-                {
-                    name: 'Steppe',
-                    type: 'bar',
-                    label: labelOption,
-                    data: [220, 182, 191, 234, 290]
-                },
-                {
-                    name: 'Desert',
-                    type: 'bar',
-                    label: labelOption,
-                    data: [150, 232, 201, 154, 190]
-                },
-                {
-                    name: 'Wetland',
-                    type: 'bar',
-                    label: labelOption,
-                    data: [98, 77, 101, 99, 40]
-                }
-            ]
-        };
-        // 使用刚指定的配置项和数据显示图表。
-        myChart2.setOption(optionBar);
 
     },
     methods:{
